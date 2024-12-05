3959dc5ba3151e3319568faeb4cffdd230be016c
efrasiccha <efrainsicchasql@gmail.com>
2024-12-04 21:50:13 -0500

    actualizando mi main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Verificar si el usuario está logueado al cargar la página
+window.onload = function() {
+    if (localStorage.getItem("isLoggedIn") === "true") {
+        // Si el usuario está logueado, ocultar los botones de Crear Cuenta e Ingresar
+        document.querySelector('.header-buttons').style.display = 'none';
+    }
+};
+
 window.addEventListener('load', function () {
     const bannerImage = document.getElementById('bannerImage');
     
